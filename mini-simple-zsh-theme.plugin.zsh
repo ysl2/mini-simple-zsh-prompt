92d4cee3976fea4d1c6c084696b5bc70ee919744
ysl2 <songli.yu@outlook.com>
2021-06-18 13:13:11 +0800

    change prompt style: add new line

--- mini-simple-zsh-theme.plugin.zsh
+++ mini-simple-zsh-theme.plugin.zsh
@@ -3,10 +3,11 @@ autoload -Uz vcs_info
 precmd() { vcs_info }
 
 # Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats ' < on branch %b'
+zstyle ':vcs_info:git:*' formats 'on branch %b'
 
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-# PROMPT='%n in ${PWD/#$HOME/~} ${vcs_info_msg_0_} > '
-PROMPT='%n in ${PWD/#$HOME/~} > '
-RPROMPT='${vcs_info_msg_0_}'
+
+
+PROMPT='%n in ${PWD/#$HOME/~} ${vcs_info_msg_0_}
+> '
